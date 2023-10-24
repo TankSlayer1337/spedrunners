6e0cd3b6dc2a056e0176dea4194d73a7802b5ce6
André Säll <erikandresall@gmail.com>
2023-10-24 11:10:15 +0200

    Add link to IMDb and Font Awesome icon.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # spedrunners
 Keep track of movies we've seen.
+
+## Tools
+
+### Font Awesome
+Font Awesome is used to display icons such as the one for external links.  
+
+Setup was made according to https://fontawesome.com/v6/docs/web/use-with/react/.  
+
+Icons are added individually explicitly: https://fontawesome.com/v6/docs/web/use-with/react/add-icons#add-individual-icons-explicitly.
--- vite-project/src/movies/MovieList.tsx
+++ vite-project/src/movies/MovieList.tsx
@@ -1,4 +1,6 @@
 import { MovieEntry } from "./movie-entry";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 
 const MovieList = ({ movies, onDelete }: { movies: MovieEntry[], onDelete: Function }) => {
 
@@ -16,6 +18,7 @@ const MovieList = ({ movies, onDelete }: { movies: MovieEntry[], onDelete: Funct
     <li key={movie.movieId}>
       <h3>{movie.title}</h3>
       <p>Vald av {movie.pickedBy}</p>
+      {movie.imdbLink && <a href={movie.imdbLink} target="_blank">IMDb <FontAwesomeIcon icon={faArrowUpRightFromSquare}></FontAwesomeIcon></a>}
       <p>Lades till {movie.created}</p>
       <button onClick={() => onDelete(movie.movieId)}>Delete</button>
     </li>
